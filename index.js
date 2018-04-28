303fea5f5ced56b9c952a137c7de5e9183ec1619
erezt.dev <erezt.dev@gmail.com>
2018-04-28 21:09:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+let fs = require('fs');
+
+app.get('/allCities', (request, response) => {
+    fs.readFile('./cityList.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        let cityNameArr = [];
+        let obj = JSON.parse(data);
+        let city;
+        for (city of obj) {
+            cityNameArr.push(city.name)
+        }
+        response.send(cityNameArr);
+    });
+
+});
+
+app.get('/city', (request, response) => {
+    fs.readFile('./cityList.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        let cityArr = [];
+        let obj = JSON.parse(data);
+        let city;
+        for (city of obj) {
+            if (city.name === 'Huntsville') {
+                cityArr.push({"country": city.country});
+            }
+        }
+        // console.log(cityArr);
+        response.send(cityArr);
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+});
